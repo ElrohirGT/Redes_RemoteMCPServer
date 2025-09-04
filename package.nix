9b8f66161622c3bbda748248bca2d38121967a70
ElrohirGT <elrohirgt@gmail.com>
2025-09-03 20:46:03 -0600

    fix: Nix package

--- package.nix
+++ package.nix
@@ -3,11 +3,10 @@
   buildGoModule,
 }:
 buildGoModule {
-  name = "Redes_MCPServer";
+  name = "Redes_RemoteMCPServer";
   src = ./.;
   # vendorHash = "sha256-fTP/PZXcJUuDx3OA2zJSTqGTwcIAJI7qXeWlCit9f+k=";
   vendorHash = "sha256-8Vs08PkqO/voV08HThCr7lwQ7AeEa501qhgBTwWlxB4=";
-  doCheck = false;
   meta = {
     description = "FAGD MCP Server for Redes course";
     homepage = "https://github.com/ElrohirGT/Redes_MCPServer";
